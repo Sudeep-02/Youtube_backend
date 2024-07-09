af8877ac3f95d2849b9dfca6a1577770fcbbaad4
Sudeep <Sudeep.chavan2000@gmail.com>
2024-07-09 11:33:32 +0530

    Database Connected

---
+++ db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n Mongo DB connceted ! DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connraction error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
