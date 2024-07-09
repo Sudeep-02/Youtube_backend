2460ef0f67132b755c307000fab62ecbe38d8ac4
Sudeep <Sudeep.chavan2000@gmail.com>
2024-07-09 13:30:06 +0530

    Utilities updated

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to start server:", err.message);
+    process.exit(1);
+  });
 
 /*
 import express from "express";
